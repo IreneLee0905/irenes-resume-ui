f6855bde30b59f4dadeed79159695e57fcd9bd74
IreneLee0905 <f228300k@gmail.com>
2019-07-24 22:34:10 +1000

    fix

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+//
+// const express = require('express');
+// const webpack = require('webpack');
+// const webpackDevMiddleware = require('webpack-dev-middleware');
+// const port = process.env.PORT || 3000; // important
+// const app = express();
+// const config = require('./webpack.config.js');
+// const compiler = webpack(config);
+//
+// // Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// // configuration file as a base.
+//
+// app.use(webpackDevMiddleware(compiler, {
+//   publicPath: config.output.publicPath
+// }));
+//
+//
+// // Serve the files on port 3000.
+// app.listen(port, function () {
+//   console.log(`Example app listening on ${port} port !`);
+// });
 
 const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const port = process.env.PORT || 3000; // important
-const app = express();
-const config = require('./webpack.config.js');
-const compiler = webpack(config);
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
-}));
-
-
-// Serve the files on port 3000.
-app.listen(port, function () {
-  console.log(`Example app listening on ${port} port !`);
-});
+express()
+  .use(express.static(path.join(__dirname, 'dist')))
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
