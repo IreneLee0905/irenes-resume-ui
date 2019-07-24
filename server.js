67b807bb924e117da4e0ddf335a257f7cda24979
IreneLee0905 <f228300k@gmail.com>
2019-07-24 22:10:03 +1000

    fix

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.use(webpackDevMiddleware(compiler, {
 
 // Serve the files on port 3000.
 app.listen(port, function () {
-  console.log(`Example app listening on $port port !`);
+  console.log(`Example app listening on ${port} port !`);
 });
