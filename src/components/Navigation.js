6101b41b1f7bd36ae330714685f200f4747126c6
IreneLee0905 <f228300k@gmail.com>
2019-08-23 22:43:31 +1000

    fix nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,40 +2,30 @@ import React from 'react';
 
 import {Link, Route} from "react-router-dom";
 import AppContextProvider from './../provider/AppContextProvider';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 class Navigation extends React.Component {
 
   render() {
     return (
       <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-dark">
-          <Link className="navbar-brand" to={"/"}>Irene's Portfolio</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-                  aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <Link className="nav-link text-right" to={"/"}>Home</Link>
-              </li>
-              <li className="nav-item active">
-                <Link className="nav-link text-right" to={"/resume"}>Resume</Link>
-              </li>
-              <li className="nav-item active">
-                <Link className="nav-link text-right" to={"/home"}>About Me</Link>
-              </li>
-              <li className="nav-item active">
-                <Link className="nav-link text-right" to={"/login"}>Login</Link>
-              </li>
-              <li className="nav-item active">
-                <Link className="nav-link text-right" to={"/register"}>Register</Link>
-              </li>
-            </ul>
 
-          </div>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" variant="dark">
+          <Navbar.Brand ><Link className="nav-link" style={{color: "rgba(255,255,255,1)"}} to={"/"}>Irene's Portfolio</Link></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+            <Nav >
+              <Nav.Link ><Link className="nav-link" style={{color: "rgba(255,255,255,1)"}} to={"/"}>Home</Link></Nav.Link>
+              <Nav.Link ><Link className="nav-link" style={{color: "rgba(255,255,255,1)"}} to={"/resume"}>Resume</Link></Nav.Link>
+              <Nav.Link ><Link className="nav-link" style={{color: "rgba(255,255,255,1)"}} to={"/"}>About Me</Link></Nav.Link>
+              <Nav.Link ><Link className="nav-link" style={{color: "rgba(255,255,255,1)"}} to={"/login"}>Login</Link></Nav.Link>
+              <Nav.Link ><Link className="nav-link" style={{color: "rgba(255,255,255,1)"}} to={"/Register"}>Register</Link></Nav.Link>
+            </Nav>
 
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     )
   }
